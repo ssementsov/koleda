10228508f8424037997a16b0d5a6cbfcb4c12d67
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-19 19:13:33 +0300

    Hello, World!

---
+++ Course_Csharp/01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        //TODO
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
