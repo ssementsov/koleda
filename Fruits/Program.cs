5ddef0d8f43f564a83e253e2562c4567fad55abe
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-23 17:10:13 +0300

    добавлены фрукты и пример

---
+++ Fruits/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fruits
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            //int pear = 8;
+            //double apple = 5.5;
+            //double fruits = pear + apple;
+            //Console.WriteLine("Имеется  " + fruits+ "  фруктов");
+            //{
+            //    int consoleColor;
+
+            //    for (consoleColor = 0; consoleColor <= 15; consoleColor++)
+            //    {
+            //        Console.ForegroundColor = (ConsoleColor)consoleColor;
+
+            //        Console.WriteLine("Foreground color set to " + (ConsoleColor)consoleColor);
+            //    }
+            //    Console.ReadKey();
+            //}
+            {
+                string template = "Background color set to ";
+                Array colors = Enum.GetValues(typeof(ConsoleColor));
+
+                foreach (ConsoleColor color in colors)
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(template + color.ToString());
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
