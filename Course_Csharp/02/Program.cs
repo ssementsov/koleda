2e8be2d9d3528236e461205cefbd7cc162771758
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-26 12:47:03 +0300

    word+

---
+++ Course_Csharp/02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string s = Console.ReadLine();
+            Console.WriteLine(s + "+");
+            Console.ReadKey();
+        }
+    }
+}
