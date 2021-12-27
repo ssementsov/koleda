023e36f55c88b685b9ec05bd1ff049cfcfd61a1c
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-27 15:22:40 +0300

    Tasks 8-10

---
+++ Course_Csharp/10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5 + double.NaN);
+            Console.WriteLine(5 - double.NaN);
+            Console.WriteLine(5 * double.NaN);
+            Console.WriteLine(5 / double.NaN);
+            Console.WriteLine(5 % double.NaN);
+            Console.WriteLine(0f / 0);
+            Console.WriteLine(float.PositiveInfinity / float.PositiveInfinity);
+            Console.WriteLine(0 * float.PositiveInfinity);
+            Console.WriteLine(float.PositiveInfinity + float.NegativeInfinity);
+            Console.WriteLine(Math.Sqrt(-4));
+            Console.WriteLine(Math.Log(-4));
+        }
+    }
+}
