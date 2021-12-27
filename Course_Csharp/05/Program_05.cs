90bf66174d3e6b316b06a056e143f419a9101acb
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-27 11:58:01 +0300

    Task 2-6

---
+++ Course_Csharp/05/Program_05.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05
+{
+    class Program_05
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.BackgroundColor = (ConsoleColor)i;
+                Console.WriteLine($"Background color set to {Console.BackgroundColor}                   ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
