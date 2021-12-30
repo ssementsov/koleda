a2e0f9ca2c39983bf2dfd3fee2be368d945400bf
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-30 13:37:24 +0300

    Task_06_Two_projects

---
+++ Two projects/Two/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("World!");
+            Console.ReadKey();
+        }
+    }
+}
