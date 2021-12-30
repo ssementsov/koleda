ea7cf9f7a60e6602eef52937847990ec221618c2
KoledaSergey <74777877+KoledaSergey@users.noreply.github.com>
2021-12-30 13:31:01 +0300

    Tasks_2-3-5

---
+++ 002/Task_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово");
+            string s = Console.ReadLine();
+            Console.WriteLine(s + "+");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 002/Task_03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, s;
+            Console.WriteLine("Введите первое слагаемое и сумму");
+            a = int.Parse(Console.ReadLine());
+            s = int.Parse(Console.ReadLine());
+            b = s - a;
+            Console.WriteLine(s + "=" + a + "+" + b);
+            Console.ReadKey();
+        }
+    }
+}
